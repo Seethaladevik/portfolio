5bb77a9df450ea4c4c0b73bac04cd4f17f32cf55
Seethaladevik <deviseethala62@gmail.com>
2025-08-08 16:21:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ showImage(current);
 let menuIcon =document.querySelector('#menu-icon');
 let navbar =document.querySelector('.navbar');
 menuIcon.onclick=()=>{
-  // menuIcon.classList.toggle('bx-x');
+  menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 };
 
